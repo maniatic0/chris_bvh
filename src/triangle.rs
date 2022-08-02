c3f8f20304551d969c95d122e025dc14cfc17448
Christian Oliveros <christianol_01@hotmail.com>
2022-08-02 21:09:41 +0200

    Indexing Triangles

--- src/triangle.rs
+++ src/triangle.rs
@@ -1,5 +1,7 @@
 extern crate glam;
 
+use std::ops::{Index, IndexMut};
+
 use rand::{
     distributions::{Distribution, Standard},
     Rng,
@@ -51,6 +53,58 @@ impl Distribution<Triangle> for Standard {
     #[inline]
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Triangle {
         Triangle::new(rng.gen(), rng.gen(), rng.gen())
+    }
+}
+
+impl Index<usize> for Triangle {
+    type Output = glam::Vec3A;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        assert!(index < 3, "Out of bounds indexing");
+        match index {
+            0 => &self.vertex0,
+            1 => &self.vertex1,
+            2 => &self.vertex2,
+            _ => panic!("Out of bounds indexing"),
+        }
+    }
+}
+
+impl Index<u8> for Triangle {
+    type Output = glam::Vec3A;
+
+    fn index(&self, index: u8) -> &Self::Output {
+        assert!(index < 3, "Out of bounds indexing");
+        match index {
+            0 => &self.vertex0,
+            1 => &self.vertex1,
+            2 => &self.vertex2,
+            _ => panic!("Out of bounds indexing"),
+        }
+    }
+}
+
+impl IndexMut<usize> for Triangle {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        assert!(index < 3, "Out of bounds indexing");
+        match index {
+            0 => &mut self.vertex0,
+            1 => &mut self.vertex1,
+            2 => &mut self.vertex2,
+            _ => panic!("Out of bounds indexing"),
+        }
+    }
+}
+
+impl IndexMut<u8> for Triangle {
+    fn index_mut(&mut self, index: u8) -> &mut Self::Output {
+        assert!(index < 3, "Out of bounds indexing");
+        match index {
+            0 => &mut self.vertex0,
+            1 => &mut self.vertex1,
+            2 => &mut self.vertex2,
+            _ => panic!("Out of bounds indexing"),
+        }
     }
 }
 
